f2db31ea58d992241fcb188c734b00f414141c85
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-05-23 11:13:07 -0300

    adicionando validacao campos vazios

--- js/form.js
+++ js/form.js
@@ -2,11 +2,25 @@ const formBtn = document.querySelector(".form__btn");
 const closeBtn = document.querySelector(".dialog__btn");
 const dialog = document.querySelector(".dialog");
 
+const emailInput = document.querySelector("#email");
+const nameInput = document.querySelector("#nome");
+const messageInput = document.querySelector("#mensagem");
+
 formBtn.addEventListener("click", function (e) {
-    dialog.showModal();
+    e.preventDefault()
+
+    if ((emailInput.value != "") && (nameInput.value != "") && (messageInput.value != "")) {
+        dialog.showModal();
+    }
+    else {
+        document.querySelector(".error").innerHTML = "Preencha todos os campos"
+    }
 });
 
 closeBtn.addEventListener("click", function () {
     dialog.close();
     window.location.reload();
 });
+
+
+
